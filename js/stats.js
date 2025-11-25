3fa64e70168e3442537a1b3fbbce9cce9f889bc4
Iain Ross <iain.ross2027@gmail.com>
2025-11-25 00:29:58 -0700

    fixed bug, i really need to test more

--- js/stats.js
+++ js/stats.js
@@ -4,15 +4,16 @@ import {sumDay} from "./calculations.js";
 function calcBestDay(){
     let bestDay = null;
     let bestDayPts = -Infinity;
-
     for(let dayIndex= 0; dayIndex<days.length; dayIndex++){
         const day = days[dayIndex];
         let dayPts = sumDay(dayIndex);
-        if(dayPts > bestDay){
+
+        if(dayPts > bestDayPts){
             bestDay = day;
             bestDayPts = dayPts;
         }
     }
+    console.log(bestDay, bestDayPts);
     return {bestDay, bestDayPts}
 }
 
